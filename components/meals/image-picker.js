e6152b44f11c0f062229c8509b1f89d78bf6147d
Raman Goyal <ramangoyal@MacBook-Air.local>
2024-03-03 01:07:16 +0530

    chore: add image picker component

---
+++ components/meals/image-picker.js
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRef } from "react";
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const imageInputRef = useRef();
+
+  function handlePickClick() {
+    if (imageInputRef) {
+      imageInputRef?.current?.click();
+    }
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          name={name}
+          accept="image/png, image/jpeg"
+          ref={imageInputRef}
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
