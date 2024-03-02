68f91c14d1c4d57993ecc5fc39166574ed3815d1
Raman Goyal <ramangoyal@MacBook-Air.local>
2024-03-02 22:56:32 +0530

    feat: add meals page template and layout setup

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,18 +1,25 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "./meals-grid";
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <p>
-        <Link href="/meals/share">Meal Share</Link>
-      </p>
-      <p>
-        <Link href="/meals/1">Meal 1</Link>
-      </p>
-      <p>
-        <Link href="/meals/2">Meal 2</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meal, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/share"> Share your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
